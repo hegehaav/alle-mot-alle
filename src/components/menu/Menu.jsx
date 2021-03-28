9c2454c65e801da9ed3b8d3284fdcd97881cf0f2
Hege <hegehaavaldsen@gmail.com>
2021-03-28 23:32:49 +0200

    Center petals

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,19 +1,42 @@
 import React from 'react';
-import './menu.scss'
+import './menu.scss';
 
 const Menu = ({ scrollTo, featuredMatchRef, scoreboardRef, tournamentRef }) => (
-    <section className="menu">
+    <section className='menu'>
         <div className='title--container'>
             <h1 className='title'>BEKK MOT BEKK</h1>
         </div>
-        <div className="menu--container">
-            <div className="menu--flower">
-                <div onClick={() => scrollTo(featuredMatchRef)} className="menu--petal p1"><span><p>Neste kamp</p></span></div>
-                <div onClick={() => scrollTo(scoreboardRef)} className="menu--petal p3"><span><p>Resultater</p></span></div>
-                <div onClick={() => scrollTo(tournamentRef)} className="menu--petal p5"><span><p>Turnering</p></span></div>
+        <div className='menu--container'>
+            <div className='menu-flower--container'>
+                <div className='menu--flower'>
+                    <div
+                        onClick={() => scrollTo(featuredMatchRef)}
+                        className='menu--petal p1'
+                    >
+                        <span>
+                            <p>Neste kamp</p>
+                        </span>
+                    </div>
+                    <div
+                        onClick={() => scrollTo(scoreboardRef)}
+                        className='menu--petal p3'
+                    >
+                        <span>
+                            <p>Resultater</p>
+                        </span>
+                    </div>
+                    <div
+                        onClick={() => scrollTo(tournamentRef)}
+                        className='menu--petal p5'
+                    >
+                        <span>
+                            <p>Turnering</p>
+                        </span>
+                    </div>
+                </div>
             </div>
         </div>
     </section>
-)
+);
 
-export default Menu; 
+export default Menu;
